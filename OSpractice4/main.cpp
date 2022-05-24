dbcd1853f4684d8c621d737324c867db617e2e1f
Dolphin <100800702+8Fixik8@users.noreply.github.com>
2022-05-24 17:03:53 +0300

    Update main.cpp

--- OSpractice4/main.cpp
+++ OSpractice4/main.cpp
@@ -2,12 +2,12 @@
 #include <time.h>
 
 int main() {
-    int64_t a = 0;
-    int b = 2;
-    int c = 3;
+    long a = 0;
+    long b = 2;
+    long c = 3;
     clock_t start = clock();
     for (int i = 0; i <= 100000000; i++)
-        a = a + (b * 2 + c - i);
+        a += b * 2 + c - i;
 
     clock_t end = clock() - start;
     std::cout << " a = " << a << "\n";
